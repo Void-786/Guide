b176a178cecb903dbd4ab255c48c6ee12fd0b060
Faiz Ullah Khan <faizullahkhan2209@gmail.com>
2025-03-22 21:00:26 +0530

    Initialised project

---
+++ guide_backend/src/main/java/com/project/guide_backend/GuideBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.guide_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuideBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuideBackendApplication.class, args);
+    }
+
+}
